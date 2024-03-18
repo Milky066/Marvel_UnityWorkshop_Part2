448f81f28d73510e33726210139478efa33ae63d
Milky066 <sahassapol@gmail.com>
2024-03-18 16:19:45 +0800

    Loading function

--- Assets/Scripts/Player/ClimbControllerWithoutScriptableObject.cs
+++ Assets/Scripts/Player/ClimbControllerWithoutScriptableObject.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class ClimbControllerWithoutScriptableObject : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [Header("Animation 1")]
+    [SerializeField] string animationName;
+    [SerializeField] float minHeight;
+    [SerializeField] float maxHeight;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [Header("Target Matching")]
+    [SerializeField] bool isTargetMatchingEnable = true;
+    [SerializeField] AvatarTarget matchingTargetBodyPart;
+    [SerializeField] float matchingTargetStartPercentage;
+    [SerializeField] float matchingTargetEndPercentage;
+
+
+    [Header("Animation 2")]
+    [SerializeField] string animationName2;
+    [SerializeField] float minHeight2;
+    [SerializeField] float maxHeight2;
+
+    [Header("Target Matching")]
+    [SerializeField] bool isTargetMatchingEnable2 = true;
+    [SerializeField] AvatarTarget matchingTargetBodyPart2;
+    [SerializeField] float matchingTargetStartPercentage2;
+    [SerializeField] float matchingTargetEndPercentage2;
+    [Header("Animation 3")]
+    [SerializeField] string animationName3;
+    [SerializeField] float minHeight3;
+    [SerializeField] float maxHeight3;
+
+    [Header("Target Matching")]
+    [SerializeField] bool isTargetMatchingEnable3 = true;
+    [SerializeField] AvatarTarget matchingTargetBodyPart3;
+    [SerializeField] float matchingTargetStartPercentage3;
+    [SerializeField] float matchingTargetEndPercentage3;
+    
 }
--- Assets/Scripts/Player/PlayerSaveLoader.cs
+++ Assets/Scripts/Player/PlayerSaveLoader.cs
@@ -1,11 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
-public class PlayerSaveLoader : MonoBehaviour
+[System.Serializable]
+public static class PlayerSaveLoader
 {
-    public void SetPlayerPosition(Vector3 position)
+    // For saving player position, Vector3 is not serializable
+    public class PlayerData
     {
-        transform.position = position;
+        public float positionX;
+        public float positionY;
+        public float positionZ;
+
+        public PlayerData(Vector3 position)
+        {
+            positionX = position.x;
+            positionY = position.y;
+            positionZ = position.z;
+        }
     }
+    private static string savePath = "./Saves/playerSave.json";
+    public static void SaveProgressJson()
+    {
+        PlayerData playerData = new PlayerData(PlayerController.instance.transform.position);
+        string jsonData = JsonUtility.ToJson(playerData);
+
+        FileStream fileStream = new FileStream(savePath, FileMode.Create);
+        StreamWriter writer = new StreamWriter(fileStream);
+        writer.Write(jsonData);
+        writer.Close();
+        fileStream.Close();
+
+        Debug.Log("Saved");
+    }
+
+    public static void LoadProgressJson()
+    {
+        FileStream fileStream = new FileStream(savePath, FileMode.Open);
+        StreamReader reader = new StreamReader(fileStream);
+        string jsonData = reader.ReadToEnd().ToString();
+        PlayerData playerData = JsonUtility.FromJson<PlayerData>(jsonData);
+        Vector3 loadPosition = new Vector3(playerData.positionX, playerData.positionY, playerData.positionZ);
+        PlayerController.instance.transform.position = loadPosition;
+        Physics.SyncTransforms();
+        reader.Close();
+        fileStream.Close();
+        Debug.Log(string.Format("Loaded at {0}", loadPosition));
+    }
+
 }
